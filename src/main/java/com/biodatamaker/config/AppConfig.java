82937e01e941e2d705d237dcc84f9f495e11b8dd
Pradeep Vishwakarma <xaier310@gmail.com>
2026-04-25 16:45:00 +0530

    Fixed WebClient maximum size tranfer to 10 mb

--- src/main/java/com/biodatamaker/config/AppConfig.java
+++ src/main/java/com/biodatamaker/config/AppConfig.java
@@ -2,6 +2,7 @@ package com.biodatamaker.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
@@ -9,6 +10,12 @@ public class AppConfig {
 
     @Bean
     public WebClient.Builder webClientBuilder() {
-        return WebClient.builder();
+        final int size = 10 * 1024 * 1024; // 10 MB -> Maximum pdf file size that can be downloaded
+        final ExchangeStrategies strategies = ExchangeStrategies.builder()
+                .codecs(codecs -> codecs.defaultCodecs().maxInMemorySize(size))
+                .build();
+
+        return WebClient.builder()
+                .exchangeStrategies(strategies);
     }
 }
